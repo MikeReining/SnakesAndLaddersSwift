6c752ef80017a047bbdb2171aa1356b0c1a315a8
Mike Reining <emailmike@gmail.com>
2015-01-18 08:16:27 -0800

    Added enum for difficulty.  Added function to startGameWithSize and Difficulty which setups new board

--- SnakesAndLaddersSwift/SnakesAndLadders.playground/section-1.swift
+++ SnakesAndLaddersSwift/SnakesAndLadders.playground/section-1.swift
@@ -3,65 +3,75 @@
 import Foundation
 
 
-enum Square: String {
+enum Square  {
     
-    case Empty = "-"
-    case Snake = "S"
-    case Ladder = "L"
+    case Empty
+    case Snake
+    case Ladder
     
-    var description: String {
-        get {
-            switch self {
-            case .Empty:
-                return "Empty\n"
-            case .Snake:
-                return "Treasure\n"
-            case .Ladder:
-                return "Ladder\n"
-            }
+    func printDescription() -> String {
+        switch self {
+        case .Empty:
+            return "Empty"
+        case .Snake:
+            return "Snake"
+        case .Ladder:
+            return "Ladder"
         }
     }
 }
 
 // Setup defaultSquare which should always be blank
-var defaultSquare = Square(rawValue: "-")
+var defaultSquare = Square.Empty
+defaultSquare.printDescription()
 
-enum Difficulty: String {
+
+enum Difficulty {
     
-    case Easy = "-"
-    case Medium = "S"
-    case Hard = "L"
+    case Easy
+    case Medium
+    case Hard
     
-    var description: String {
-        get {
-            switch self {
-            case .Easy:
-                return "Easy\n"
-            case .Medium:
-                return "Medium\n"
-            case .Hard:
-                return "Hard\n"
-            }
+    func printDescription() -> String {
+        switch self {
+        case .Easy:
+            return "Easy"
+        case .Medium:
+            return "Medium"
+        case .Hard:
+            return "Hard"
         }
     }
 }
-
 
 class GameBoard {
     var rows: Int
     var columns: Int
     var square: Square
     var board: [Square]
-    init (size: Int) {
+    var difficulty: Difficulty
+    init (size: Int, difficulty: Difficulty) {
         self.rows = size
         self.columns = size
-        self.square = defaultSquare!
+        self.square = defaultSquare
+        self.difficulty = difficulty
         board = Array(count: rows * columns, repeatedValue: square)
+    }
+    func startNewGameWithSize(size: Int, withDifficulty: Difficulty) {
+        self.rows = size
+        self.columns = size
+        board = Array(count: rows * columns, repeatedValue: square)
+        self.difficulty = withDifficulty
     }
 }
 
 
 // Setup default board array with blank squares
-var gameBoard = GameBoard(size: 5)
+var gameBoard = GameBoard(size: 5, difficulty: .Easy)
+
+gameBoard.startNewGameWithSize(10, withDifficulty: .Medium)
+
+
+
 
 
