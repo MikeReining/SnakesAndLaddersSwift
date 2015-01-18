e01d3563e82de7d29cb268023701a3667d79958f
Mike Reining <emailmike@gmail.com>
2015-01-18 09:05:43 -0800

    Added function to put ladders in random position in Array

--- SnakesAndLaddersSwift/SnakesAndLadders.playground/section-1.swift
+++ SnakesAndLaddersSwift/SnakesAndLadders.playground/section-1.swift
@@ -50,7 +50,6 @@ class GameBoard {
         default:
             println("Wrong difficulty entered")
         }
-        
         self.rows = size
         self.columns = size
         board = Array(count: rows * columns, repeatedValue: 0)
@@ -58,7 +57,36 @@ class GameBoard {
     }
 }
 
-
 // Setup default board array with blank squares
 var gameBoard = GameBoard(size: 5, difficulty: .Easy)
 
+
+// Step 1 - Put ladders in random positions in grid
+func generateRandomPositionForLadders() -> [Int] {
+    var laddersCount = Int(Double(gameBoard.board.count) * gameBoard.percentLadders)
+    var laddersArray = [Int]()
+    for ladder in 1...laddersCount {
+        var randomSquare = Int(arc4random_uniform(UInt32(gameBoard.board.count)))
+        while randomSquare == 0 || randomSquare == gameBoard.board.count || randomSquare == (gameBoard.board.count - 1) {
+            randomSquare = Int(arc4random_uniform(UInt32(gameBoard.board.count)))
+        }
+        laddersArray.append(randomSquare)
+        
+    }
+    return laddersArray
+}
+generateRandomPositionForLadders()
+
+
+
+
+
+
+
+
+
+var snakesCount = Int(Double(gameBoard.board.count) * gameBoard.percentLadders)
+
+
+
+// Step 2 - Calculate random boost value for snakes
