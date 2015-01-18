056d9fcad525bc04144f18fce5e141b7b7ac7b93
Mike Reining <emailmike@gmail.com>
2015-01-18 08:42:49 -0800

    Added switch statement to change mix of snakes and ladders based on difficulty level

--- SnakesAndLaddersSwift/SnakesAndLadders.playground/section-1.swift
+++ SnakesAndLaddersSwift/SnakesAndLadders.playground/section-1.swift
@@ -25,13 +25,32 @@ class GameBoard {
     var columns: Int
     var board: [Int]
     var difficulty: Difficulty
+    var percentSnakes: Double
+    var percentLadders: Double
     init (size: Int, difficulty: Difficulty) {
         self.rows = size
         self.columns = size
         self.difficulty = difficulty
+        self.percentLadders = 0.17
+        self.percentSnakes = 0.17
         board = Array(count: rows * columns, repeatedValue: 0)
     }
+    
     func startNewGameWithSize(size: Int, withDifficulty: Difficulty) {
+        switch withDifficulty {
+        case .Easy:
+            self.percentLadders = 0.25
+            self.percentSnakes = 0.1
+        case .Medium:
+            self.percentLadders = 0.17
+            self.percentSnakes = 0.17
+        case .Hard:
+            self.percentLadders = 0.1
+            self.percentSnakes = 0.25
+        default:
+            println("Wrong difficulty entered")
+        }
+        
         self.rows = size
         self.columns = size
         board = Array(count: rows * columns, repeatedValue: 0)
@@ -42,8 +61,4 @@ class GameBoard {
 
 // Setup default board array with blank squares
 var gameBoard = GameBoard(size: 5, difficulty: .Easy)
-
-gameBoard.startNewGameWithSize(10, withDifficulty: .Medium)
-
-
 
